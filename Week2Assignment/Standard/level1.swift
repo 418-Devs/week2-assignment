d93d619624312ee1bbdd31daf715d8849f0d24de
Gwangyong <key4168@icloud.com>
2025-06-04 14:35:14 +0900

    feat: standard level1 구현 및 main 연결

--- Week2Assignment/Standard/level1.swift
+++ Week2Assignment/Standard/level1.swift
@@ -7,5 +7,16 @@
 
 struct Level1 {
     static func run() {
+        let sum: (Int, Int) -> String = { a, b in
+            return "두 수의 합은 \(a + b)입니다."
+        }
+        
+        // sum 계산 방식을 calculate 함수에 전달
+        calculate(closure: sum)
+    }
+    
+    static func calculate(closure: (Int, Int) -> String) {
+        // 내부에서 sum(3, 7) 호출과 동일
+        print(closure(3, 7))
     }
 }

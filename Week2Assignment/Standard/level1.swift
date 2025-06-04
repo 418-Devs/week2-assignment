91c203e0df7d69a69fd33d58ed1e5bd9d8ae4e35
Gwangyong <key4168@icloud.com>
2025-06-04 16:14:35 +0900

    chore: standard level1 주석 수정

--- Week2Assignment/Standard/level1.swift
+++ Week2Assignment/Standard/level1.swift
@@ -7,14 +7,16 @@
 
 struct Level1 {
     static func run() {
-        let sum: (Int, Int) -> String = { a, b in
-            return "두 수의 합은 \(a + b)입니다."
+        // 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저
+        let sum: (Int, Int) -> String = {
+            return "두 수의 합은 \($0 + $1)입니다."
         }
         
         // sum 계산 방식을 calculate 함수에 전달
         calculate(closure: sum)
     }
     
+    // sum과 동일한 타입의 클로저를 파라미터로 받고, 반환 값이 없는(void) 함수
     static func calculate(closure: (Int, Int) -> String) {
         // 내부에서 sum(3, 7) 호출과 동일
         print(closure(3, 7))

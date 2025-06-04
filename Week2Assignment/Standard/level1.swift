d4acc32c0ede1035cd51f70dbc18ccbf7ef78a26
Gwangyong <key4168@icloud.com>
2025-06-04 17:12:32 +0900

    chore: 구조와 출력문 약간 수정

--- Week2Assignment/Standard/level1.swift
+++ Week2Assignment/Standard/level1.swift
@@ -7,6 +7,12 @@
 
 struct Level1 {
     static func run() {
+        print("👉 [필수 문제 1] 클로저를 매개변수로 전달하여 두 수의 합 계산")
+        level1()
+    }
+    
+    // 클로저를 매개변수로 전달하여 두 수의 합 계산
+    private static func level1() {
         // 두 개의 Int 값을 파라미터로 받고, 하나의 String 값을 반환하는 클로저
         let sum: (Int, Int) -> String = {
             return "두 수의 합은 \($0 + $1)입니다."
@@ -17,7 +23,7 @@ struct Level1 {
     }
     
     // sum과 동일한 타입의 클로저를 파라미터로 받고, 반환 값이 없는(void) 함수
-    static func calculate(closure: (Int, Int) -> String) {
+    private static func calculate(closure: (Int, Int) -> String) {
         // 내부에서 sum(3, 7) 호출과 동일
         print(closure(3, 7))
     }

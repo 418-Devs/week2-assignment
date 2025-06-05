3a73072fc61ceb490e8b53004e22589a74dc1baf
Yoon-Mac <sryoowk@gmail.com>
2025-06-05 15:25:43 +0900

    메인 진입부 가독성 수정

--- Week2Assignment/main.swift
+++ Week2Assignment/main.swift
@@ -9,17 +9,17 @@ import Foundation
 
 let activeTask = 5
 
-switch activeTask {
-case 1:
-    main1()
-case 2:
-    main2()
-case 3:
-    main3()
-case 4:
-    main4()
-case 5:
-    main5()
-default:
+let taskMap: [Int: () -> Void] = [
+    1: main1, //  필수문제 1
+    2: main2, //  필수문제 2
+    3: main3, //  필수문제 3
+    4: main4, //  필수문제 4
+    5: main5, //  필수문제 5
+    6: main6  // 도전문제 1
+]
+
+if let task = taskMap[activeTask] {
+    task()
+} else {
     print("유효한 과제 번호가 아닙니다.")
 }

4c36cded623da9809f21c6698635f4bc8fa17d5e
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-06-09 10:56:54 +0900

    도전문제 1번 클래스 정의

--- Week2Assignment/Challenges/Challenge1.swift
+++ Week2Assignment/Challenges/Challenge1.swift
@@ -7,5 +7,27 @@
 import Foundation
 
 class Challenge1 {
+
+    
+    
+}
+
+class Car {
+    var brand: String
+    var model: String
+    var year: String
+    
+    init(brand: String, model: String, year: String) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+}
+
+class ElectricCar: Car {
+    
+}
+
+class HybridCar: Car {
     
 }

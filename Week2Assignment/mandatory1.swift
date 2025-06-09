02abda03bf2ebb28115e43dc8bbaf9002d5a8083
Yoon-Mac <sryoowk@gmail.com>
2025-06-09 12:55:09 +0900

    필수문제 1 리팩토링

--- Week2Assignment/mandatory1.swift
+++ Week2Assignment/mandatory1.swift
@@ -11,7 +11,7 @@ import Foundation
 // 두 개의 Int값을 파라미터로 받고 하나의 String 값을 반환하는 클로저를 설계
 // 상수 sum에 저장하고 정확한 타입을 명시
 let sum: (Int, Int) -> String = {
-    "두 수의 합은 \($0 + $1)입니다."
+    "두 수의 합은 \($0 + $1) 입니다."
 }
 
 // sum과 동일한 타입의 클로저를 파라미터로 받고 반환값이 Void인 함수
@@ -29,5 +29,5 @@ func main1() {
     calculate(1, 9, with: sum)
 
     // 후행 클로저를 사용한 calculate함수 호출
-    calculate(10, 20) { "두 수의 곱은 \($0 * $1)입니다" }
+    calculate(10, 20) { "두 수의 곱은 \($0 * $1) 입니다" }
 }
